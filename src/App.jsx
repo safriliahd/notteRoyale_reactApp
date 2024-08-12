4c64ed0047ef2ef4e083588994241bedea1fa06f
rhevagiant <122246574+rhevagiant@users.noreply.github.com>
2024-08-12 14:09:42 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
       <h1>hello notte royale</h1>
       <h1 style={{ color: primary[100] }}>hello notte royale</h1>
       <p>tes repo</p>
+      <h1>tes repo 2</h1>
     </>
   )
 }
