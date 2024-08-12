7522b7044fbca182510015e8b7d256b100c324b4
safriliahd <safrilianurhidayah@gmail.com>
2024-08-12 13:52:25 +0700

    awal

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { primary } from './theme/color'
+
+
+function App() {
+
+  return (
+    <>
+      <h1>hello notte royale</h1>
+      <h1 style={{ color: primary[100] }}>hello notte royale</h1>
+    </>
+  )
+}
+
+export default App
