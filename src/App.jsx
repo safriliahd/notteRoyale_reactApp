8982623bf376f5581dae9bea4bacd643cf717dc0
nazilahnaina <143465114+nazilahnaina@users.noreply.github.com>
2024-09-03 12:50:44 +0700

    update path signUp

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RouterAdmin from './router/routerAdmin/view';
 import PageSingIn from './screen/signIn/view';
-import DataProductCRUD from './screen/admin-product-crud/view';
+import PageSignUp from './screen/signUp/view';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(true); // This is just a mock state
