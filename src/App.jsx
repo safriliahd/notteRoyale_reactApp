3caa11d06215cd20c4fc599af9df497ff08e0bae
safriliahd <safrilianurhidayah@gmail.com>
2024-09-03 12:39:11 +0700

    update signIn-signUp

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RouterAdmin from './router/routerAdmin/view';
 import PageSingIn from './screen/signIn/view';
+import PageSignUp from './screen/signUp/view';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(true); // This is just a mock state
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<PageSingIn />} />
         <Route path="/*" element={isLoggedIn ? <RouterAdmin /> : <PageSingIn />} />
+        <Route path="/sign-up" element={<PageSignUp />}/>
       </Routes>
     </Router>
   );
